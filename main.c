7d33c51606530c648cb4859b3f862b6cf54dfcef
Chorismate <me@chorismate.dev>
2023-03-16 17:42:45 -0700

    basic main.c and main.h

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "main.h"
+
+#include "delays.h"
+#include "theoutset.h"
+
+
+
+int main(void) {
+	fputs("Welcome to Unknown.\n", stdout);
+	return 0;
+}
--- main.h
+++ main.h
@@ -1,4 +1,6 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+const char *VERSION = "0.1 alpha";
+
 #endif
